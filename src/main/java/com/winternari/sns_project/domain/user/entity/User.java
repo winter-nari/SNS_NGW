c3e9ccb906177eeb07fe7c2ce6cac86e80ae1ab6
adula <whitenora807@gmail.com>
2025-07-29 18:11:00 +0900

    first commit

---
+++ src/main/java/com/winternari/sns_project/domain/user/entity/User.java
@@ -0,0 +1,5 @@
+package com.winternari.sns_project.domain.user.entity;
+
+public class User {
+
+}
--- src/main/java/com/winternari/sns_project/domain/user/service/UserService.java
+++
@@ -1,4 +0,0 @@
-package com.winternari.sns_project.domain.user.service;
-
-public interface UserService {
-}
